9b1a31a63274faff47c6011e837bbbbeaced17bb
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-24 13:48:33 -0400

    Ignoring and removing congig from repo

--- config/config.py
+++
@@ -1,2 +0,0 @@
-KAFKA_NODES = ['ec2-54-84-42-80.compute-1.amazonaws.com:9092', 'ec2-18-211-13-85.compute-1.amazonaws.com:9092',
-               'ec2-52-0-129-251.compute-1.amazonaws.com:9092', 'ec2-18-215-20-238.compute-1.amazonaws.com:9092']
