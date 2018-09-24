f4b5e21c00a0b9423df52a86299811acaa7343cf
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-24 17:40:56 -0400

    '

--- spark/streaming.py
+++ spark/streaming.py
@@ -3,7 +3,7 @@ import json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-import redis
+#import redis
 KAFKA_NODES = ['ec2-54-84-42-80.compute-1.amazonaws.com:9092', 'ec2-18-211-13-85.compute-1.amazonaws.com:9092',
                'ec2-52-0-129-251.compute-1.amazonaws.com:9092', 'ec2-18-215-20-238.compute-1.amazonaws.com:9092']
 
