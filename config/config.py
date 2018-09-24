37e04b5f710c4302bf6665f0bce364a419f17908
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-24 13:38:32 -0400

    ''

--- config/config.py
+++ config/config.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 KAFKA_NODES = ['ec2-54-84-42-80.compute-1.amazonaws.com:9092', 'ec2-18-211-13-85.compute-1.amazonaws.com:9092',
                'ec2-52-0-129-251.compute-1.amazonaws.com:9092', 'ec2-18-215-20-238.compute-1.amazonaws.com:9092']
-=======
-KAFKA_NODES = []
->>>>>>> 8aba91a3dbcf3ec92a1fab140d4ad02bdaadcaa9
