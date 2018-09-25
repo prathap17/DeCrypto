e9ac451f99b659535dace88ae558232d2d08ee3f
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-25 11:05:12 -0400

    ""Batch

--- spark/spark_main.py
+++ spark/spark_main.py
@@ -1,18 +1,31 @@
 from streaming import SparkStreamConsumer
+import json
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+KAFKA_NODES = ['ec2-54-84-42-80.compute-1.amazonaws.com:9092', 'ec2-18-211-13-85.compute-1.amazonaws.com:9092',
+               'ec2-52-0-129-251.compute-1.amazonaws.com:9092', 'ec2-18-215-20-238.compute-1.amazonaws.com:9092']
+
+
+
 
 if __name__ == '__main__':
     # Kraken asset pairs for BTC, ETH, and LTC to USD prices
-    asset_pairs = ['BTC-USD', 'ETH-USD']
-    intervals = [1, 5, 15, 30, 60, 240, 1440, 10080, 21600]
-    spread_topics = []
-    ohlc_topics = []
+    
 
-    for asset_pair in asset_pairs:
-        # Spread
-        topic_name = '{}_{}'.format('Coinbase', 'Spread')
-        spread_topics.append(topic_name)
-    print(spread_topics)
+    #consumer = SparkStreamConsumer()
+    #consumer.consume_spreads(['Coinbase'])
+    #consumer.start_stream()
+    sc = SparkContext(appName='SparkStream', master='spark://ec2-54-84-42-80.compute-1.amazonaws.com:7077')
+    ssc = StreamingContext(sc)
+    kvs = KafkaUtils.createDirectStream(ssc, ['Coinbase'],
+                                                 {'metadata.broker.list': ','.join(KAFKA_NODES)})
+        # messages come in [timestamp, bid, ask] format
+    parsed = kvs.map(lambda v: json.loads(v[1]))
+        
 
-    consumer = SparkStreamConsumer()
-    consumer.consume_spreads(['Coinbase'])
-    consumer.start_stream()
+        
+    parsed.pprint()
+
+    
+
