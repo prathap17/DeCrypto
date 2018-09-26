eedfb2c6107f6e3e386c060b7ae18641918a5e5f
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-26 13:41:59 -0400

    "Modified streaming data"

--- spark/spark_main.py
+++ spark/spark_main.py
@@ -1,32 +1,7 @@
-import json
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-KAFKA_NODES = ['ec2-54-84-42-80.compute-1.amazonaws.com:9092', 'ec2-18-211-13-85.compute-1.amazonaws.com:9092',
-               'ec2-52-0-129-251.compute-1.amazonaws.com:9092', 'ec2-18-215-20-238.compute-1.amazonaws.com:9092']
-
-
-
+from streaming import SparkStreamConsumer
 
 if __name__ == '__main__':
-    sc = SparkContext(appName='SparkStream', 
-                      master='spark://ec2-54-84-42-80.compute-1.amazonaws.com:7077')
-    sc.setLogLevel("ERROR")
 
-
-    ssc = StreamingContext(sc, 1)
-    kvs = KafkaUtils.createDirectStream(ssc, ['Coinbase'],
-                                                 {'metadata.broker.list': ','.join(KAFKA_NODES)})
-        # messages come in [timestamp, bid, ask] format
-    parsed = kvs.map(lambda v: json.loads(v[1]))
-        
-
-    print("The data is")    
-    parsed.pprint()
-
-    ssc.start()
-    ssc.awaitTermination()
-
-
-    
-
+    consumer = SparkStreamConsumer()
+    consumer.consume_spreads(['Coinbase'])
+    consumer.start_stream()
--- spark/streaming.py
+++ spark/streaming.py
@@ -3,34 +3,52 @@ import json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-#import redis
-KAFKA_NODES = ['ec2-54-84-42-80.compute-1.amazonaws.com:9092', 'ec2-18-211-13-85.compute-1.amazonaws.com:9092',
-               'ec2-52-0-129-251.compute-1.amazonaws.com:9092', 'ec2-18-215-20-238.compute-1.amazonaws.com:9092']
+import redis
 
 
-def print_partition(time, rdd):
-    for msg in rdd:
-        print('this is data', msg)
-class SparkStreamConsumer :
-    def __init__(self):#, slide_interval=5, window_length=15):
-        self.sc = SparkContext(appName='SparkStream', master='spark://ec2-54-84-42-80.compute-1.amazonaws.com:7077')
-        self.ssc = StreamingContext(self.sc)#, slide_interval)
-        #self.slide_interval = slide_interval
-        #self.window_length = window_length
+
+# These redis commands need to be defined outside of a class as it is passed in a dstream
+r = redis.StrictRedis(host='', port=6379, db=0)
+
+
+def set_redis_avg_spread(partition):
+    for msg in partition:
+        r.hset(msg[0], 'avg_spread', msg[1])
+
+
+def set_redis_bid_ask(partition):
+    for msg in partition:
+        r.hset(msg[3], 'bid', msg[1])
+        r.hset(msg[3], 'ask', msg[2])
+        r.hset(msg[3], 'spread', Decimal(msg[2]) - Decimal(msg[1]))
+
+
+class SparkStreamConsumer:
+    def __init__(self, slide_interval=5, window_length=15):
+        self.sc = SparkContext(appName='SparkStream', master='spark://.compute-1.amazonaws.com:7077')
+        self.ssc = StreamingContext(self.sc, slide_interval)
+        self.slide_interval = slide_interval
+        self.window_length = window_length
 
     def start_stream(self):
         self.ssc.start()
         self.ssc.awaitTermination()
 
     def consume_spreads(self, spread_topics):
-    
         self.kvs = KafkaUtils.createDirectStream(self.ssc, spread_topics,
                                                  {'metadata.broker.list': ','.join(KAFKA_NODES)})
         # messages come in [timestamp, bid, ask] format
-        parsed = self.kvs.map(lambda v: json.loads(v[1])).cache()
-        #parsed = self.kvs.window(self.window_length, self.slide_interval).map(lambda v: json.loads(v[1])).cache()
+        parsed = self.kvs.window(self.window_length, self.slide_interval).map(lambda v: json.loads(v[1])).cache()
 
-        
-        #parsed.pprint()
+        # set the current bid and ask prices in Redis
+        parsed.foreachRDD(lambda rdd: rdd.foreachPartition(set_redis_bid_ask))
 
-        parsed.foreachRDD((print_partition))
+        # calculate the spread of each transaction and append a count value
+        # input: [timestamp, bid, ask, asset_pair] output: (asset_pair, spread, 1)
+        def calculate_spread(tx):
+            asset_pair = tx[3]
+            return (asset_pair, Decimal(tx[2]) - Decimal(tx[1]))
+
+        spread_percentage_dstream = parsed.map(calculate_spread).mapValues(lambda x: (x, 1))
+
+        print(spread_percentage_dstream)
