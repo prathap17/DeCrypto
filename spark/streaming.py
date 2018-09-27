ec1ead838f5ee6494b0e7ed76c373d343899097f
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-26 21:30:20 -0400

    "modified for redis"

--- spark/streaming.py
+++ spark/streaming.py
@@ -1,31 +1,29 @@
+
+
+
+
 from decimal import Decimal
 import json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+#import redis
 from pyspark.streaming.kafka import KafkaUtils
-import redis
+KAFKA_NODES = []
 
+r = redis.StrictRedis(host='redis://localhost', port=6379, db=0)
 
-
-# These redis commands need to be defined outside of a class as it is passed in a dstream
-r = redis.StrictRedis(host='', port=6379, db=0)
-
-
-def set_redis_avg_spread(partition):
+def set_redis_data(partition):
     for msg in partition:
-        r.hset(msg[0], 'avg_spread', msg[1])
-
-
-def set_redis_bid_ask(partition):
-    for msg in partition:
-        r.hset(msg[3], 'bid', msg[1])
-        r.hset(msg[3], 'ask', msg[2])
-        r.hset(msg[3], 'spread', Decimal(msg[2]) - Decimal(msg[1]))
+        print("It is in the function___________________________________________")
+        print(msg)
+        (r.hsetnx(msg[3], 'bid', msg[1]))
+        print("redis-----------------------------------------------------------data")
+        r.hsetnx(msg[3], 'ask', msg[2])
 
 
 class SparkStreamConsumer:
     def __init__(self, slide_interval=5, window_length=15):
-        self.sc = SparkContext(appName='SparkStream', master='spark://.compute-1.amazonaws.com:7077')
+        self.sc = SparkContext(appName='SparkStream', master='spark://ec2-18-235-136-124.compute-1.amazonaws.com:7077')
         self.ssc = StreamingContext(self.sc, slide_interval)
         self.slide_interval = slide_interval
         self.window_length = window_length
@@ -38,17 +36,13 @@ class SparkStreamConsumer:
         self.kvs = KafkaUtils.createDirectStream(self.ssc, spread_topics,
                                                  {'metadata.broker.list': ','.join(KAFKA_NODES)})
         # messages come in [timestamp, bid, ask] format
-        parsed = self.kvs.window(self.window_length, self.slide_interval).map(lambda v: json.loads(v[1])).cache()
 
-        # set the current bid and ask prices in Redis
-        parsed.foreachRDD(lambda rdd: rdd.foreachPartition(set_redis_bid_ask))
+        parsed = self.kvs.window(self.window_length, self.slide_interval).map(lambda v: json.loads(v[1]))
 
-        # calculate the spread of each transaction and append a count value
-        # input: [timestamp, bid, ask, asset_pair] output: (asset_pair, spread, 1)
-        def calculate_spread(tx):
-            asset_pair = tx[3]
-            return (asset_pair, Decimal(tx[2]) - Decimal(tx[1]))
+        print("the data is")
 
-        spread_percentage_dstream = parsed.map(calculate_spread).mapValues(lambda x: (x, 1))
-
-        print(spread_percentage_dstream)
+        parsed.foreachRDD(lambda rdd: rdd.foreachPartition(set_redis_data))
+        print("data-------------------------------------is passed")
+  
+          
+       
