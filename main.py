5aa13200afc0c371618fb6b4333fd270b14040e0
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-29 17:18:00 -0400

    "Added Bitrex

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from ingestion.src.main import start_coinbase_producer
 from ingestion.src.main import start_bitfinex_producer
+from ingestion.src.main import start_kraken_producer
 
 
 
 
 if __name__ == '__main__':
-    start_coinbase_producer()
+    #start_coinbase_producer()
     #start_bitfinex_producer()
-    
+    start_kraken_producer()
     
