1bfb24896f33975ccceacaa1e37188ebae5dea68
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-30 16:32:41 -0400

    "

--- ingestion/src/main.py
+++ ingestion/src/main.py
@@ -1,10 +1,13 @@
 import time
 from ccxt import cex
 from ccxt import bitstamp
+from ccxt import livecoin
 
 from ingestion.src.coinbase_pro import CoinbasePro
 from ingestion.src.cex import Cex
 from ingestion.src.bitstamp import Bitstamp
+from ingestion.src.livecoin import LiveCoin
+
 def start_coinbase_producer():
     wsClient = CoinbasePro()
     wsClient.start()
@@ -17,9 +20,16 @@ def start_cex_producer():
         data.produce()
 
 def start_bitstamp_producer():
-    products =["BTC/USD", "ETH/USD", "BCH/USD"]  
+    products =["BTC/USD"]  
     
     for product in products:
-        data = Bitstamp( product, cex().fetch_order_book(product, 10))
+        data = Bitstamp( product, bitstamp().fetch_order_book(product))
+        data.produce()
+
+def start_livecoin_producer():
+    products =["BTC/USD"]  
+    
+    for product in products:
+        data = LiveCoin( product, livecoin().fetch_order_book(product, 10))
         data.produce()
 
