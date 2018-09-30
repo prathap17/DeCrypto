c167baa73ab83b693345a61b851ae2040c104f3c
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-30 15:01:16 -0400

    "Modified for CEX.io

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ingestion.src.main import start_coinbase_producer
-from ingestion.src.main import start_bitfinex_producer
-from ingestion.src.main import start_kraken_producer
+
+from ingestion.src.main import start_cex_producer
 
 
 
@@ -8,5 +8,5 @@ from ingestion.src.main import start_kraken_producer
 if __name__ == '__main__':
     #start_coinbase_producer()
     #start_bitfinex_producer()
-    start_kraken_producer()
+    start_cex_producer()
     
