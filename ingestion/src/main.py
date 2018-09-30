a3f13de95e06440544c9a271bc3a9c9a91b9712e
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-30 15:02:02 -0400

    "Modified for Cex.io

--- ingestion/src/main.py
+++ ingestion/src/main.py
@@ -1,12 +1,16 @@
 import time
+from ccxt import cex
 
 from ingestion.src.coinbase_pro import CoinbasePro
-from ingestion.src.bitfinex import BitfinexProducer
-
+from ingestion.src.cex import Cex
 def start_coinbase_producer():
     wsClient = CoinbasePro()
     wsClient.start()
 
-def start_bitfinex_producer():
-    wsClient = BitfinexProducer()
-    wsClient.start()
+def start_cex_producer():
+    products =["BTC/USD", "ETH/USD", "BCH/USD"]  
+    
+    for product in products:
+        data = Cex( product, cex().fetch_order_book(product, 10))
+        data.produce()
+
