8340cc488bff562604b5675da25f058141ca498b
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-09-30 15:28:24 -0400

    Bitstamp

--- ingestion/src/main.py
+++ ingestion/src/main.py
@@ -1,8 +1,10 @@
 import time
 from ccxt import cex
+from ccxt import bitstamp
 
 from ingestion.src.coinbase_pro import CoinbasePro
 from ingestion.src.cex import Cex
+from ingestion.src.bitstamp import Bitstamp
 def start_coinbase_producer():
     wsClient = CoinbasePro()
     wsClient.start()
@@ -12,5 +14,12 @@ def start_cex_producer():
     
     for product in products:
         data = Cex( product, cex().fetch_order_book(product, 10))
+        data.produce()
+
+def start_bitstamp_producer():
+    products =["BTC/USD", "ETH/USD", "BCH/USD"]  
+    
+    for product in products:
+        data = Bitstamp( product, cex().fetch_order_book(product, 10))
         data.produce()
 
