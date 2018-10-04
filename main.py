c6d16eb5ca8edc2aa768049fc2308e0e6356b474
Prathap Reddy Keesari <prathapkreddy17@gmail.com>
2018-10-04 18:14:15 -0400

    "

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from ingestion.src.main import start_coinbase_producer
 from ingestion.src.main import start_cex_producer
 from ingestion.src.main import start_bitstamp_producer
+from ingestion.src.main import start_livecoin_producer
+from ingestion.src.main import start_bitstamp_producer
 
 
 
 
 if __name__ == '__main__':
-    #start_coinbase_producer()
-    #start_bitfinex_producer()
+    start_coinbase_producer()
     #start_cex_producer()
-    start_bitstamp_producer()
+    #start_bitfinex_producer()
+    #while True:
+        #start_bitstamp_producer()
+        #start_livecoin_producer()
+        #start_cex_producer()
+        #start_bitstest_producer()
     
